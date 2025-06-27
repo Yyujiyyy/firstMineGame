0d0049bc278dd46b5c56ef014bcca0f9edd923cd
yuji2005yy <yuji2005yy@gmail.com>
2025-06-27 17:47:15 +0900

    [Improve] TopGreyImageの上以外で反応するように修正

--- Assets/script/PressAnyButton.cs
+++ Assets/script/PressAnyButton.cs
@@ -5,29 +5,46 @@ using UnityEngine.SceneManagement;
 public class PressAnyButton : MonoBehaviour
 {
     private InputAction _pressAnyKeyAction;
-    [SerializeField] private string sceneName;          //GameScene
 
-    private void OnEnable()
+    [SerializeField] private string sceneName;       // GameScene
+    [SerializeField] private GameObject targetUI;    // 無視したいUI
+
+    private bool isPointerOverUI = false;
+
+    private void Awake()
     {
         // 任意のボタン入力を検知するアクションを作成
         _pressAnyKeyAction = new InputAction(type: InputActionType.PassThrough, binding: "*/<Button>", interactions: "Press");
-
         // 入力があったときのイベントハンドラ登録
         _pressAnyKeyAction.performed += OnAnyButtonPressed;
-
-        // アクションを有効化
-        _pressAnyKeyAction.Enable();
+        _pressAnyKeyAction.Enable(); // 常に有効化（ただし実行はホバー中のみ）
     }
 
-    private void OnDisable()
+    private void Update()
     {
-        // 無効化＆イベント解除（メモリリーク防止）
-        _pressAnyKeyAction.performed -= OnAnyButtonPressed;
-        _pressAnyKeyAction.Disable();
+        // マウスが targetUI 上にあるかを毎フレーム確認
+        if (targetUI != null)
+        {
+            isPointerOverUI = RectTransformUtility.RectangleContainsScreenPoint
+            (
+                targetUI.GetComponent<RectTransform>(),
+                Mouse.current.position.ReadValue(),
+                null // Camera を使う場合は Camera.main にする
+            );
+        }
     }
 
     private void OnAnyButtonPressed(InputAction.CallbackContext context)
     {
-        SceneManager.LoadScene(sceneName);
+        if (!isPointerOverUI)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    private void OnDisable()
+    {
+        _pressAnyKeyAction.performed -= OnAnyButtonPressed;
+        _pressAnyKeyAction.Disable();
     }
 }
