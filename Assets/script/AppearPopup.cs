d7d9f2adf7fd9527caf066700f229130947a17bc
yuji2005yy <yuji2005yy@gmail.com>
2025-06-29 00:02:38 +0900

    [Update] 感度調節を入力で変更できるように変更

--- Assets/script/AppearPopup.cs
+++ Assets/script/AppearPopup.cs
@@ -7,7 +7,7 @@ public class AppearPopup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        settingPopup.SetActive(false);
     }
 
     // Update is called once per frame
--- Assets/script/SensitivitySlider.cs
+++ Assets/script/SensitivitySlider.cs
@@ -5,6 +5,9 @@ public class SensitivitySlider : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI SensitivityText;
     [SerializeField] private UnityEngine.UI.Slider sensitivitySlider; // スライダー参照
+    [SerializeField] private TMP_InputField inputFieldSensitivity; // ← 追加：数値入力欄
+
+    public FPScamera cameraScript; // FPSカメラスクリプト参照
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +19,44 @@ public class SensitivitySlider : MonoBehaviour
 
         // スライダーとテキストに反映
         sensitivitySlider.value = savedSensitivity;
-        SensitivityText.text = "Sensitivity : " + savedSensitivity.ToString("F2");
-    }
+        UpdateSensitivityDisplay(savedSensitivity);
 
-    public FPScamera cameraScript; // FPSカメラスクリプト参照
+        // イベントリスナー登録
+        sensitivitySlider.onValueChanged.AddListener(OnSpeedChanged);
+        inputFieldSensitivity.onEndEdit.AddListener(OnInputFieldChanged);
+
+        //SensitivityText.text = "Sensitivity : " + savedSensitivity.ToString("F2");
+    }
 
     public void OnSpeedChanged(float value)
     {
         PlayerPrefs.SetFloat("Sensitivity", value); // 感度保存
-        SensitivityText.text = "Sensitivity : " + value.ToString("F2");     //小数点第３位を四捨五入して文字列に変換
+        UpdateSensitivityDisplay(value);
 
         if (cameraScript != null)
         {
             cameraScript.Sensitivity = value; // ここで即反映
         }
+    }
+
+        private void OnInputFieldChanged(string input)
+    {
+        if (float.TryParse(input, out float value))
+        {
+            value = Mathf.Clamp(value, sensitivitySlider.minValue, sensitivitySlider.maxValue);
+            sensitivitySlider.value = value; // ← 自動でOnSpeedChangedが呼ばれます
+        }
+        else
+        {
+            // 無効な入力ならスライダーの値を再表示
+            inputFieldSensitivity.text = sensitivitySlider.value.ToString("F2");
+        }
+    }
+
+    private void UpdateSensitivityDisplay(float value)
+    {
+        string display = value.ToString("F2");
+        SensitivityText.text = "Sensitivity : " + display;
+        inputFieldSensitivity.text = display;
     }
 }
