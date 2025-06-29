6624d68079e53eb9c6106151fa077d5131c3c6eb
yuji2005yy <yuji2005yy@gmail.com>
2025-06-30 02:04:36 +0900

    [Update] F1でタイトルに戻るように変更

--- Assets/script/F1ToTitleGo.cs
+++ Assets/script/F1ToTitleGo.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EscapeToTitleGo : MonoBehaviour
+public class F1ToTitleGo : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.F1))
         {
             SceneManager.LoadScene(0);
             // Build Settingsで一番上のシーンをロードする
