61ff7571ccd2a6b40108d58ab5844e0da93c6eec
yuji2005yy <yuji2005yy@gmail.com>
2025-07-07 20:20:25 +0900

    [Add] 初期ボットにスポーンポイントの正しい設定を実装

--- Assets/_Script/NeutralEnemy.cs
+++ Assets/_Script/NeutralEnemy.cs
@@ -6,16 +6,17 @@ public class NeutralEnemy : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab;          //EnemyのPrefab
     [SerializeField] List<Transform> spawnPoints;
     List<Transform> availablePoints;
-
-    [SerializeField] public int enemyCount;
+    [SerializeField, Range(1, 25)] private int enemyCount = 5; // 生成数をインスペクターで指定
 
     // Start is called before the first frame update
     void Start()
     {
+        // スポーン地点をコピーして使用
         availablePoints = new List<Transform>(spawnPoints);
 
-        int enemyCount = Mathf.Min(24, availablePoints.Count);
-        for (int i = 0; i < enemyCount; i++)
+        // 敵生成数はスポーン地点の数を超えないようにする
+        int spawnCount = Mathf.Min(enemyCount, availablePoints.Count);
+        for (int i = 0; i < spawnCount; i++)
         {
             int index = Random.Range(0, availablePoints.Count);
             Transform spawnPoint = availablePoints[index];
