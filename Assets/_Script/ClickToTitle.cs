680b65268b60c8824fc05bf2322a81c73816b356
yuji2005yy <yuji2005yy@gmail.com>
2025-07-10 00:54:09 +0900

    [Add] タイトル画面に戻るボタンを追加

---
+++ Assets/_Script/ClickToTitle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToTitle : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
